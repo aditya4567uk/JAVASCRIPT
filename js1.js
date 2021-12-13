9b6d4b6ea0507506dcd0f561ccdca7d11f21743f
Aditya Kumar <86232529+aditya4567uk@users.noreply.github.com>
2021-12-13 22:34:44 +0530

    Add files via upload

---
+++ js1.js
@@ -0,0 +1,41 @@
+//ways to print a statement
+console.log("Aditya");
+alert("Aditya here!!");
+document.write("i'm Aditya");
+
+//JS Console API
+console.log("hello world",4+6);
+console.warn("This will give YELLOW warning in JS console");
+console.error("This will give RED warning in JS console");
+
+//JS Variables
+var num1=67;
+var num2=89;
+console.log("Addition is\t"+num1+num2);
+
+var str1 = "This is string";
+
+var marks = {  //making Objects
+    s1=90,
+    s2=91,
+    s3=68.8,
+}
+console.log(marks);
+
+/*
+MULTILINE COMMENT
+*/
+
+//boolean datatype
+var a =true;
+var b =false;
+console.log(a);
+console.log(b);
+
+var und = undefined; //declared a variable und but value is undefined
+var und1; //same thing as done in above line
+
+//array
+var arr=[10,20,30,40,50,60,70,80,90,"String1","string2"];
+console.log(arr[6]);
+
